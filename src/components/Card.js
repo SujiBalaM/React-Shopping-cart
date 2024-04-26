5ca3953a9c0e45dffb17b028304b60450bc825d9
Sujatha Manoharan <Sujathamanoharan7@gmail.com>
2024-04-26 15:34:09 -0700

    Firestore and bootstrap tailwind added

---
+++ src/components/Card.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import { mydatabase } from '../firebase'
+
+const Card = () => {
+
+    const [productData,setProductData] = useState([])
+    useEffect(()=>{
+        mydatabase.collection("products").onSnapshot((snapshot) => {
+            setProductData(snapshot.docs.map((i) =>{
+                return i.data()
+            }))
+        })
+    })
+  return (
+    <div className='card-container'>
+        {
+            productData.map((i) => {
+                return   <div key={i.slno} className="card" style={{width: 350}}>
+                <img src={i.imageurl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{i.name}</h5>
+                  <p className="card-text">{i.description} </p>
+                  <del><h5>Price:{i.price}</h5></del>
+                  <h5>Discounted Rate:{i.discount}</h5>
+                  <a href="#" className="btn btn-primary">Add to cart</a>
+                </div>
+              </div>
+            })
+        }
+      
+    </div>
+  )
+}
+
+export default Card
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+import firebase from "firebase/compat/app"
+import  "firebase/compat/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyBqyCB_AOO4HjpCmYjiP_vxC7fEOPlAHLo",
+  authDomain: "react-shopping-cart-18425.firebaseapp.com",
+  projectId: "react-shopping-cart-18425",
+  storageBucket: "react-shopping-cart-18425.appspot.com",
+  messagingSenderId: "228498220224",
+  appId: "1:228498220224:web:50e113f7229d3dbf1a7925"
+};
+const app = firebase.initializeApp(firebaseConfig)
+export const mydatabase = firebase.firestore();
